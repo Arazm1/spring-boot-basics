1cc366bc081495565910f0ce1241a0535e70de2a
Arazm1 <araz.muhamm3d@gmail.com>
2026-05-09 18:26:17 +0300

    Fixed the script.sql, env variables now work.

--- script.sql
+++ script.sql
@@ -2,12 +2,10 @@
 create database springbootbasics1
 
 --Create table basic--
-
 CREATE TABLE basic (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    note TEXT NOT NULL
-)
-
+    note VARCHAR(255) NOT NULL
+);
 
 INSERT INTO basic (note) VALUES ('Spring Boot is pretty cool');
 INSERT INTO basic (note) VALUES ('JPA makes database stuff easier');
