b826f20d87a5debe5362802177ffaee7e499a6ee
Arazm1 <araz.muhamm3d@gmail.com>
2026-05-09 17:35:44 +0300

    Basic sql script done.

---
+++ script.sql
@@ -0,0 +1,14 @@
+--Create the database--
+create database springbootbasics1
+
+--Create table basic--
+
+CREATE TABLE basic (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    note TEXT NOT NULL
+)
+
+
+INSERT INTO basic (note) VALUES ('Spring Boot is pretty cool');
+INSERT INTO basic (note) VALUES ('JPA makes database stuff easier');
+INSERT INTO basic (note) VALUES ('MariaDB and MySQL are very similar');
