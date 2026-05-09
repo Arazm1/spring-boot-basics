a3b61038647a715194d0f7bce1fe39a634e3b589
Arazm1 <araz.muhamm3d@gmail.com>
2026-05-09 17:36:25 +0300

    Expanded the code by adding entity class Basic. In addition, restructure.

---
+++ projectone/src/main/java/entity/Basic.java
@@ -0,0 +1,31 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "basic")
+public class Basic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String note;
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getNote(){
+        return this.note;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setNote(String note){
+        this.note = note;
+    }
+   
+}
